8bb0324e012b3bd635e4bdc3d2a91d6bf13abfdb
Archit laptop <archit.sharma1@ucalgary.ca>
2026-03-25 12:48:06 -0600

    Add multiple imports needed for the project. Will add more as needed

---
+++ Data221FinalProjectMainCode.py
@@ -0,0 +1,7 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+import tensorflow as tf
